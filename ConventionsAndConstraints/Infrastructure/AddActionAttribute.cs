4c944d025d9d7b0823dfd5a30a2efde9dd703a7f
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-08-03 17:26:54 +0200

    Finished Constraints and Convetions chapter example.

---
+++ ConventionsAndConstraints/Infrastructure/AddActionAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConventionsAndConstraints.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class AddActionAttribute : Attribute
+    {
+        public string AdditionalName { get; }
+
+        public AddActionAttribute(string name)
+        {
+            AdditionalName = name;
+        }
+    }
+
+    public class AdditionalActionsAttribute : Attribute, IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            var actions = controller.Actions.Select(x => new
+            {
+                Action = x,
+                Names = x.Attributes.Select(attr => (attr as AddActionAttribute)?.AdditionalName)
+            });
+
+            foreach (var item in actions.ToList())
+            {
+                foreach (var name in item.Names)
+                {
+                    controller.Actions.Add(new ActionModel(item.Action)
+                    {
+                        ActionName = name
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ ConventionsAndConstraints/Infrastructure/UserAgentAttribute.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace ConventionsAndConstraints.Infrastructure
+{
+    public class UserAgentAttribute : Attribute, IActionConstraintFactory
+    {
+        private string substring;
+
+        public UserAgentAttribute(string sub)
+        {
+            substring = sub.ToLower();
+        }
+
+        public bool IsReusable => false;
+
+        public IActionConstraint CreateInstance(IServiceProvider services)
+        {
+            return new UserAgentConstraint(services.GetService<UserAgentComparer>(), substring);
+        }
+    }
+    
+    public class UserAgentConstraint: IActionConstraint
+    {
+        private UserAgentComparer comparer;
+        private string substring;
+
+        public UserAgentConstraint(UserAgentComparer comp, string sub)
+        {
+            comparer = comp;
+            substring = sub.ToLower();
+        }
+
+        public int Order { get; set; } = 0;
+
+        public bool Accept(ActionConstraintContext context)
+        {
+            return context.RouteContext.HttpContext.Request.Headers["User-Agent"].Any(x => x.ToLower().Contains(substring)) || context.Candidates.Count() == 1;
+        }
+    }    
+}
